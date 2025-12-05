42f01d6535457f53d2d7c589db5f5dda85bbdbbe
MS-Haque <toshafin@gmail.com>
2025-12-05 21:56:42 +0000

    Deploying to gh-pages from @ MS-Haque/mshaque@a10dab781d6fa6a58f41c1c2340dc88f69fd9037 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,19 +37,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/mshaque/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/mshaque/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/mshaque/teaching/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/mshaque/cv/";
           },
         },{id: "nav-people",
           title: "people",
