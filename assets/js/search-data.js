bc1f7b718d7f21b9ac66b6d2782221c690af17cf
MS-Haque <toshafin@gmail.com>
2025-12-05 17:39:49 +0000

    Deploying to gh-pages from @ MS-Haque/mshaque@0cc47233f2297d0effc15db6989aed0b6dea7c39 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/mshaque/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
+        },{id: "nav-mataq-lab",
+          title: "MatAQ Lab",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
